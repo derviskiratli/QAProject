572e61b14993cc0188fecc7914f93f32a26b6c4a
Dervis Kiratli <dervis.kiratli@atos.net>
2022-04-25 14:20:17 +0200

    Added find, delete and update methods for customers

--- JavaQaBackend/src/main/java/Main.java
+++ JavaQaBackend/src/main/java/Main.java
@@ -30,8 +30,69 @@ public class Main {
                     break;
                 case 2:
                     for( Customer c : customers) {
-                        System.out.println(c.toString());
+                        System.out.println(c.toString() + "\n");
                     }
+                    break;
+                case 3:
+                    boolean found = false;
+                    System.out.println("Enter id of customer to find:");
+                    int id = scanner.nextInt();
+                    for( Customer c : customers) {
+                        if (c.getId() == id){
+                            found = true;
+                            System.out.println(c.toString());
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Customer not found");
+                    }
+                    break;
+                case 4:
+                    boolean deleted = false;
+                    Customer customerToDelete = null;
+                    System.out.println("Enter id of customer to delete:");
+                    id = scanner.nextInt();
+                    for( Customer c : customers) {
+                        if (c.getId() == id){
+                            deleted = true;
+                            customerToDelete = c;
+                        }
+                    }
+                    if (!deleted) {
+                        System.out.println("Customer not found");
+                    } else {
+                        customers.remove(customerToDelete);
+                        System.out.println("Customer deleted successfully");
+                    }
+                    break;
+                case 5:
+                    boolean updated = false;
+                    System.out.println("Enter id of customer to update:");
+                    id = scanner.nextInt();
+                    scanner.nextLine();
+                    for( Customer c : customers) {
+                        if (c.getId() == id){
+                            System.out.println("Old first name: " + c.getFirstName());
+                            System.out.println("Enter new first name");
+                            c.setFirstName(scanner.nextLine());
+                            System.out.println("Old last name: " + c.getLastName());
+                            System.out.println("Enter new last name");
+                            c.setLastName(scanner.nextLine());
+                            System.out.println("Old email: " + c.getEmail());
+                            System.out.println("Enter new email");
+                            c.setEmail(scanner.nextLine());
+                            System.out.println("Old phone number: " + c.getPhoneNumber());
+                            System.out.println("Enter new phone number");
+                            c.setPhoneNumber(scanner.nextInt());
+                            updated = true;
+                        }
+                    }
+                    if (!updated) {
+                        System.out.println("Customer not found");
+                    } else {
+                        System.out.println("Customer updated successfully");
+                    }
+                    break;
             }
         }
         while (choice!= 0);
