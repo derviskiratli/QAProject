74bd4b7fd002c0b25efd6de329244ddc1319141f
Dervis Kiratli <dervis.kiratli@atos.net>
2022-04-25 12:43:20 +0200

    Added methods to main for creating customers and showing all customers

--- JavaQaBackend/src/main/java/Main.java
+++ JavaQaBackend/src/main/java/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do{
+            System.out.println("1.Create customer");
+            System.out.println("2.Show customers");
+            System.out.println("3.Find customers");
+            System.out.println("4.Delete customer");
+            System.out.println("5.Update customer");
+            System.out.println("Enter your choice(0 to cancel): ");
+            choice= scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 1:
+                    System.out.println("Customer first name:");
+                    String fName = scanner.nextLine();
+                    System.out.println("Customer last name:");
+                    String lName = scanner.nextLine();
+                    System.out.println("Customer email:");
+                    String email = scanner.nextLine();
+                    System.out.println("Customer phone number:");
+                    int phone = scanner.nextInt();
+                    customers.add(new Customer(fName, lName, email, phone));
+                    break;
+                case 2:
+                    for( Customer c : customers) {
+                        System.out.println(c.toString());
+                    }
+            }
+        }
+        while (choice!= 0);
     }
 }
